3138c997ebd397153c4f316227be51018a91fa00
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-06-03 22:23:48 -0700

    commonCharacterCount

---
+++ array_101/commonCharacterCount/commonCharacterCount.js
@@ -0,0 +1,42 @@
+/* Given two strings, find the number of common characters between them.
+
+  Example
+
+For s1 = "aabcc" and s2 = "adcaa", the output should be
+commonCharacterCount(s1, s2) = 3.
+
+Strings have 3 common characters - 2 "a"s and 1 "c".
+
+  Input / Output
+
+  [execution time limit]4 seconds(js)
+
+  [input] string s1
+
+A string consisting of lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ s1.length < 15.
+
+[input] string s2
+
+A string consisting of lowercase English letters.
+
+Guaranteed constraints:
+1 ≤ s2.length < 15.
+
+[output] integer */
+function commonCharacterCount(s1, s2) {
+  let count = 0;
+  let a1 = s1.split('');
+  let a2 = s2.split('');
+
+  a1.forEach(e => {
+    if (a2.includes(e)) {
+      count++;
+      a2.splice(a2.indexOf(e), 1);
+    }
+  });
+
+  return count;
+}
