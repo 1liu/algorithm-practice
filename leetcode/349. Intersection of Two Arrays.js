cb83b9832d4e00fa5868c63618cc63a3d7348925
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-06-20 22:08:33 -0700

    [leetcode] Q349

---
+++ leetcode/349. Intersection of Two Arrays.js
@@ -0,0 +1,18 @@
+/*
+ * @lc app=leetcode.cn id=349 lang=javascript
+ *
+ * [349] 两个数组的交集
+ */
+
+// @lc code=start
+/**
+ * @param {number[]} nums1
+ * @param {number[]} nums2
+ * @return {number[]}
+ */
+var intersection = function(nums1, nums2) {
+  return [... new Set(nums1.filter(num=>nums2.includes(num)))];
+};
+/* all elements in set must be unique
+ */
+// @lc code=end
