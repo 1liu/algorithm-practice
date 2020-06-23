0f13db0926ace3b23aaa5b3ecfc9d457d28edf17
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-06-22 22:50:36 -0700

    [leetcode] Q36

---
+++ leetcode/36. Valid Sudoku.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=36 lang=javascript
+ *
+ * [36] 有效的数独
+ */
+
+// @lc code=start
+/**
+ * @param {character[][]} board
+ * @return {boolean}
+ */
+var isValidSudoku = function(board) {
+  let rows={}, cols={}, boxes={};
+  for (let i = 0; i< 9 ; i++){
+    for (let j = 0; j<9; j++){
+      let num = board[i][j];
+      if(num!=='.'){
+        let boxIndex = parseInt((i / 3)) * 3 + parseInt(j / 3);
+         if(rows[i+''+num]===true||cols[j+''+num]===true||boxes[boxIndex+''+num]===true){
+           return false;
+         }else{
+           rows[i + '' + num] = true;
+           cols[j + '' + num] = true;
+           boxes[boxIndex + '' + num] = true;
+         }
+      }
+    }
+  }
+  return true;
+};
+// @lc code=end
