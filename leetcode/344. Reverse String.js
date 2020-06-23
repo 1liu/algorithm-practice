3f5b337eaf52d3b83d79bb9994496081b3874ed5
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-06-23 11:18:04 -0700

    [leetcode] Q344

--- leetcode/344. Reverse String.js
+++ leetcode/344. Reverse String.js
@@ -9,6 +9,14 @@
  * @param {character[]} s
  * @return {void} Do not return anything, modify s in-place instead.
  */
+/* var reverseString = function (s) {
+  let n = s.length;
+  for (let i = 0; i < n / 2; i++) {
+    let temp = s[i];
+    s[i] = s[n - 1 - i];
+    s[n - 1 - i] = temp;
+  }
+}; */ //use temp to swap
 var reverseString = function (s) {
   let n = s.length, left = 0, right = n - 1;
   while (left < right) {
