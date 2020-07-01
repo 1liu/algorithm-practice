cc6639b733dc93a4389a02b93916be64010d17d2
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-07-01 00:02:07 -0700

    [Leetcode] Q8 Solution 3

--- leetcode/8. String to Integer (atoi).js
+++ leetcode/8. String to Integer (atoi).js
@@ -21,10 +21,36 @@
 //   }
 // };
 
-let myAtoi = (str) => {
-  let res = str.trim().match(/^(\-|\+)?\d+/i)
-  return res ? Math.max(Math.min(Number(res[0]), 2 ** 31 - 1), -(2 ** 31)) : 0
-};
+// let myAtoi = (str) => {
+//   let res = str.trim().match(/^(\-|\+)?\d+/i)
+//   return res ? Math.max(Math.min(Number(res[0]), 2 ** 31 - 1), -(2 ** 31)) : 0
+// };
+ var myAtoi = function (str) {
+   const INT_MIN = -Math.pow(2, 31);
+   const INT_MAX = Math.pow(2, 31) - 1;
+
+  let res = 0, i = 0, flag = 1;
+  while(str[i]===' ') {i++;}
+  if(str[i]==='-') flag = -1;
+  if(str[i]==='+' || str[i] === '-') i++;
+
+   while (i < str.length && str[i]!==' '&&Number.isInteger(Number(str[i]))){
+    let r = Number(str[i]);
+     if (res > INT_MAX / 10 || (res === Math.floor(INT_MAX / 10) && r > 7)){
+      return flag > 0?INT_MAX:INT_MIN;
+    }
+    res = res*10 + r;
+    i++;
+  }
+  return flag>0?res:-res;
+
+ }
+
+console.log(myAtoi("2147483648"))
+/*
+Time: O(n)
+Space: O(1)
+*/
 
 
 // @lc code=end
