d7d965c0b708683f9288c2c2133432d2bc8d8764
Yizhou Liu <yizhou.liu.mail@gmail.com>
2020-06-30 22:07:40 -0700

    [leetcode] 8. String to Integer (atoi) Answer 2

--- leetcode/8. String to Integer (atoi).js
+++ leetcode/8. String to Integer (atoi).js
@@ -9,16 +9,22 @@
  * @param {string} str
  * @return {number}
  */
-var myAtoi = function (str) {
-  const number = parseInt(str, 10);
+// var myAtoi = function (str) {
+//   const number = parseInt(str, 10);
 
-  if (isNaN(number)) {
-    return 0;
-  } else if (number < Math.pow(-2, 31) || number > Math.pow(2, 31) - 1) {
-    return number < Math.pow(-2, 31) ? Math.pow(-2, 31) : Math.pow(2, 31) - 1;
-  } else {
-    return number;
-  }
+//   if (isNaN(number)) {
+//     return 0;
+//   } else if (number < Math.pow(-2, 31) || number > Math.pow(2, 31) - 1) {
+//     return number < Math.pow(-2, 31) ? Math.pow(-2, 31) : Math.pow(2, 31) - 1;
+//   } else {
+//     return number;
+//   }
+// };
+
+let myAtoi = (str) => {
+  let res = str.trim().match(/^(\-|\+)?\d+/i)
+  return res ? Math.max(Math.min(Number(res[0]), 2 ** 31 - 1), -(2 ** 31)) : 0
 };
+
 
 // @lc code=end
